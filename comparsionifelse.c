d10fb3136b6ffbf6f1c7dc9eb45dd987d2985417
ravindra <ravindra.cloudgen.in>
2024-06-27 16:22:02 +0530

    Commit V1.0

---
+++ comparsionifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main ()
+{
+int a,b;
+printf("enter A value \n\n ");
+scanf("%d",&a);
+printf("enter B value \n\n ");
+scanf("%d",&b);
+
+if(a>b)
+printf("a is bigger than b \n\n");
+else if(a<b)
+printf("b is bigger than a \n\n");
+
+
+}
